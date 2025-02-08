7feb7a2c68e6bcecca0e2f12250f40eb86f8d1d1
TOWHID <web.development162253@gmail.com>
2025-02-09 03:19:07 +0600

    my second commit

---
+++ fast.js
@@ -0,0 +1,21 @@
+// const numbers =[12, 20, 48, 36, 19, 64]
+// console.log(numbers)
+// for (const num of numbers){
+//     console.log(num)
+// }
+
+
+// var
+// var ghorarDim = [12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64];
+// console.log(ghorarDim)
+
+// for (var kobutorerDim of ghorarDim){
+//     console.log(kobutorerDim)
+// }
+
+let  ghorarDim = [12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64];
+console.log(ghorarDim)
+
+for (let kobutorerDim of ghorarDim){
+    console.log(kobutorerDim)
+}
