7eca1505ac8c947dc271e203748d6e827e193ab7
TOWHID <web.development162253@gmail.com>
2025-02-09 05:50:12 +0600

    commit for Array

--- fast.js
+++ fast.js
@@ -13,9 +13,19 @@
 //     console.log(kobutorerDim)
 // }
 
+// let
 let  ghorarDim = [12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64, 12 , 20, 48, 36, 19, 64];
-console.log(ghorarDim)
+console.log(ghorarDim.length)
 
-for (let kobutorerDim of ghorarDim){
-    console.log(kobutorerDim)
-}
+// for (let kobutorerDim of ghorarDim){
+//     console.log(kobutorerDim)
+//     console.log(kobutorerDim.length)
+// }
+
+
+// Change the value of array using index.
+const fruits=["apple", "mango", "banana", "dragon"]
+fruits[1]="hagu";
+console.log(fruits)
+console.log(fruits[1])
+console.log(fruits.length)
